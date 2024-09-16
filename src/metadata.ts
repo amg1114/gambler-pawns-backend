3440b1266cc819de0fa40b5188d3c89c1a39b391
jfmonsa <juanmonsalve@protonmail.com>
2024-09-15 23:38:39 -0500

    merge feature chessLogic into dev

--- src/metadata.ts
+++ src/metadata.ts
@@ -1,5 +1,81 @@
 /* eslint-disable */
 export default async () => {
     const t = {};
-    return { "@nestjs/swagger": { "models": [[import("./auth/dto/auth.dto"), { "SignUpDto": { nickname: { required: true, type: () => String, minLength: 3, maxLength: 20 }, email: { required: true, type: () => String }, password: { required: true, type: () => String }, countryCode: { required: true, type: () => String } }, "LoginDto": { nickname: { required: false, type: () => String, minLength: 3, maxLength: 20 }, email: { required: false, type: () => String }, password: { required: true, type: () => String } } }], [import("./websocket/dto/joinGame.dto"), { "JoinGameDTO": { playerId: { required: true, type: () => String }, eloRating: { required: true, type: () => Number }, mode: { required: true, type: () => Object }, bet: { required: false, type: () => Number } } }], [import("./websocket/dto/makeMove.dto"), { "MakeMoveDTO": { playerId: { required: true, type: () => String }, from: { required: true, type: () => String }, to: { required: true, type: () => String } } }], [import("./websocket/dto/acceptDraw.dto"), { "AcceptDrawDTO": { playerId: { required: true, type: () => String }, gameId: { required: true, type: () => String } } }], [import("./websocket/dto/offerDraw.dto"), { "OfferDrawDTO": { playerId: { required: true, type: () => String }, gameId: { required: true, type: () => String } } }]], "controllers": [[import("./auth/auth.controller"), { "AuthController": { "signUp": {}, "login": {} } }]] } };
+    return {
+        "@nestjs/swagger": {
+            models: [
+                [
+                    import("./auth/dto/auth.dto"),
+                    {
+                        SignUpDto: {
+                            nickname: {
+                                required: true,
+                                type: () => String,
+                                minLength: 3,
+                                maxLength: 20,
+                            },
+                            email: { required: true, type: () => String },
+                            password: { required: true, type: () => String },
+                            countryCode: { required: true, type: () => String },
+                        },
+                        LoginDto: {
+                            nickname: {
+                                required: false,
+                                type: () => String,
+                                minLength: 3,
+                                maxLength: 20,
+                            },
+                            email: { required: false, type: () => String },
+                            password: { required: true, type: () => String },
+                        },
+                    },
+                ],
+                [
+                    import("./websocket/dto/joinGame.dto"),
+                    {
+                        JoinGameDTO: {
+                            playerId: { required: true, type: () => String },
+                            eloRating: { required: true, type: () => Number },
+                            mode: { required: true, type: () => Object },
+                            bet: { required: false, type: () => Number },
+                        },
+                    },
+                ],
+                [
+                    import("./websocket/dto/makeMove.dto"),
+                    {
+                        MakeMoveDTO: {
+                            playerId: { required: true, type: () => String },
+                            from: { required: true, type: () => String },
+                            to: { required: true, type: () => String },
+                        },
+                    },
+                ],
+                [
+                    import("./websocket/dto/acceptDraw.dto"),
+                    {
+                        AcceptDrawDTO: {
+                            playerId: { required: true, type: () => String },
+                            gameId: { required: true, type: () => String },
+                        },
+                    },
+                ],
+                [
+                    import("./websocket/dto/offerDraw.dto"),
+                    {
+                        OfferDrawDTO: {
+                            playerId: { required: true, type: () => String },
+                            gameId: { required: true, type: () => String },
+                        },
+                    },
+                ],
+            ],
+            controllers: [
+                [
+                    import("./auth/auth.controller"),
+                    { AuthController: { signUp: {}, login: {} } },
+                ],
+            ],
+        },
+    };
 };
