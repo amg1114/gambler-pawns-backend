6a1b764c46f5c3a8ff0886b619e594bba1e99ec6
jfmonsa <juanmonsalve@protonmail.com>
2024-09-17 18:36:14 -0500

    fix: renaming websocket folder to chess

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { DrizzleModule } from "./drizzle/drizzle.module";
 import { AuthModule } from "./auth/auth.module";
-import { GatewayModule } from "./websocket/websocket.module";
+import { GatewayModule } from "./chess/websocket.module";
 import { ServeStaticModule } from "@nestjs/serve-static";
 import { join } from "path";
 import { AssetsModule } from "./assets/assets.module";
