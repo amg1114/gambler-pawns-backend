d4ae8c931b69b71d221e06800b55d22e27b62332
Alejandro Moreno <alejandromogil@gmail.com>
2024-09-23 22:32:53 -0500

    refactor: Update getUserInfo method in UserController and UserService

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -15,7 +15,7 @@ export class UserController {
     @ApiOperation({ summary: "Get user data by nickname" })
     @ApiResponse({ status: 200, description: "User data found successfully" })
     getUsers(@Param("nickname") nickname: string): Promise<User | null> {
-        return this.userService.findOneByNickname(nickname);
+        return this.userService.getUserInfo(nickname);
     }
 
     @Patch(":id")
