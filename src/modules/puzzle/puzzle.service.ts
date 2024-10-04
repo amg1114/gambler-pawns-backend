bdbdecfcd8e8dddcada036429debf12a8d63b794
NicolasPL <31411531+NicolasPL64@users.noreply.github.com>
2024-10-04 18:55:49 -0500

    Added /puzzle/random GET endpoint

---
+++ src/modules/puzzle/puzzle.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Puzzle } from "./entities/puzzle.entity";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class PuzzleService {
+    constructor(
+        @InjectRepository(Puzzle)
+        private puzzleEntityRepository: Repository<Puzzle>,
+    ) {}
+
+    async getRandomPuzzle() {
+        const totalPuzzles = await this.puzzleEntityRepository.count();
+
+        const randomIndex = Math.round(Math.random() * totalPuzzles);
+
+        const puzzle = await this.puzzleEntityRepository
+            .createQueryBuilder()
+            .skip(randomIndex)
+            .take(1)
+            .getOne();
+
+        const { puzzleId, userSolvedPuzzles, ...puzzleData } = puzzle;
+        return puzzleData;
+    }
+}
